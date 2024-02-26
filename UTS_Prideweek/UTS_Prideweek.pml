de8fb9c38561043454cce425b932dfa757187b89
mminajjang <Mina.Jang-1@student.uts.edu.au>
2024-02-26 19:08:42 +1100

    final version

--- UTS_Prideweek/UTS_Prideweek.pml
+++ UTS_Prideweek/UTS_Prideweek.pml
@@ -10,12 +10,38 @@
         <File name="style" src="html/css/style.css" />
         <File name="uts-pride-events-hero-1" src="html/images/uts-pride-events-hero-1.jpg" />
         <File name="uts-pride-logo" src="html/images/uts-pride-logo.png" />
-        <File name="uts_prideweek_2024" src="html/images/uts_prideweek_2024.png" />
         <File name="index" src="html/index.html" />
         <File name="jquery-3.7.1.min" src="html/js/jquery-3.7.1.min.js" />
         <File name="main" src="html/js/main.js" />
         <File name="robotutils" src="html/js/robotutils.js" />
         <File name="icon" src="icon.png" />
+        <File name="BlackOpsOne-Regular" src="html/fonts/Black_Ops_One/BlackOpsOne-Regular.ttf" />
+        <File name="OFL" src="html/fonts/Black_Ops_One/OFL.txt" />
+        <File name="KodeMono-VariableFont_wght" src="html/fonts/Kode_Mono/KodeMono-VariableFont_wght.ttf" />
+        <File name="OFL" src="html/fonts/Kode_Mono/OFL.txt" />
+        <File name="README" src="html/fonts/Kode_Mono/README.txt" />
+        <File name="KodeMono-Medium" src="html/fonts/Kode_Mono/static/KodeMono-Medium.ttf" />
+        <File name="KodeMono-SemiBold" src="html/fonts/Kode_Mono/static/KodeMono-SemiBold.ttf" />
+        <File name="Micro5-Regular" src="html/fonts/Micro_5/Micro5-Regular.ttf" />
+        <File name="OFL" src="html/fonts/Micro_5/OFL.txt" />
+        <File name="OFL" src="html/fonts/Silkscreen/OFL.txt" />
+        <File name="Silkscreen-Bold" src="html/fonts/Silkscreen/Silkscreen-Bold.ttf" />
+        <File name="Silkscreen-Regular" src="html/fonts/Silkscreen/Silkscreen-Regular.ttf" />
+        <File name="activeuts_opaque" src="html/images/activeuts_opaque.png" />
+        <File name="activeuts_trans" src="html/images/activeuts_trans.png" />
+        <File name="ally" src="html/images/ally.jpg" />
+        <File name="arrow" src="html/images/arrow.png" />
+        <File name="drag_bingo" src="html/images/drag_bingo.jpg" />
+        <File name="dragbingo" src="html/images/dragbingo.png" />
+        <File name="home" src="html/images/home.png" />
+        <File name="rainbow" src="html/images/rainbow.jpg" />
+        <File name="rainbow2" src="html/images/rainbow2.jpg" />
+        <File name="studentgroup" src="html/images/studentgroup.png" />
+        <File name="uts-pride-logo (1)" src="html/images/uts-pride-logo (1).png" />
+        <File name="utslgbt_opaque" src="html/images/utslgbt_opaque.png" />
+        <File name="utslgbt_trans" src="html/images/utslgbt_trans.png" />
+        <File name="KodeMono-Bold" src="html/fonts/Kode_Mono/KodeMono-Bold.ttf" />
+        <File name="KodeMono-Regular" src="html/fonts/Kode_Mono/KodeMono-Regular.ttf" />
     </Resources>
     <Topics />
     <IgnoredPaths />
--- UTS_Prideweek/html/js/main.js
+++ UTS_Prideweek/html/js/main.js
@@ -1,13 +1,162 @@
 var audio = new Audio('change_screen.ogg');
-// var session = new QiSession();
-
-
+var session = new QiSession(function(session){
+    console.log("connected !" );
+}, function(){
+    console.log("disconnected");
+});
+var flag = true;
 $(document).ready( function(){
-    $('button').click(function(){
-        audio.play();
+    if(flag){
+        session.service("ALTextToSpeech").then(function(tts){
+            tts.setParameter("speed", 80);
+            tts.setParameter("pitchShift", 1.18);
+        });
+        flag = false;
+    }
+});
+
+var current_page_number = 1;
+var min_page_num = 1;
+var max_page_num = 3;
+var texts = ['Love Is Love! Everyone is invited! Check out Whats on! ', 'Join our Communities!', 'Check out What Events are happening!'];
+function convertPage(dir){
+    clearTimeout(timer);
+    $("#page").empty();
+    current_page_number += dir;
+    if(current_page_number > max_page_num){
+        current_page_number = min_page_num;
+    }
+    else if(current_page_number < min_page_num){
+        current_page_number = max_page_num;
+    }
+    if (current_page_number == 1){
+        $("#page").append(
+            '<div class="header_div"  style="height: 500px;">'+
+                '<h1 class="headline_2" style="margin: 0;"> LOVE</h1>'+
+                '<h1 class="headline_2" style="margin-top: 150px;"> IS</h1>'+
+                '<h1 class="headline_2" style="margin-top: 300px;"> LOVE</h1>'+
+            '</div>'+
+            '<div class="content_div" style="height:240px;">'+
+                '<p class="subheader" style="padding-top: 20px;">FACULTY OF ENGINEERING &#9825 IT</p>'+
+                '<p class="subheader" style="padding-top: 0;">UTS PRIDE WEEK 2024</p>'+
+            '</div>'
+            // '<div class="footer_div">'+
+            //     '<button class="button_home"></button>'+
+            // '</div>'
+            );
+    }
+    else if(current_page_number == 2){
+        $("#page").append(
+            '<div class="header_div" style="height: 180px;">' + 
+                '<h1 class="headline"> Join Our Communities !</h1>'+
+            '</div>'+
+            '<div class="content_div" style="height:560px;">'+
+            '<div class="container">'+
+               ' <div class="item_container" style="margin-right:80px;">'+
+                   '<div class="item_img" type="img" style="background-image: url(images/activeuts_trans.png);"></div>'+
+                    '<div class="item_title"><p>ActivateUTS</p></div>'+
+                '</div>'+
+                '<div class="item_container" style="margin-left:80px;">'+
+                   '<div class="item_img" type="img" style="background-image: url(images/utslgbt_trans.png);"></div>'+
+                   '<div class="item_title"><p>UTS LGBTIQA+ Community</p></div>'+
+                '</div>'+
+            '</div>'+
+        '</div>'
+        // '<div class="footer_div">'+
+        //     '<button class="button_home"></button>'+
+        // '</div>'
+        );
+    }
+    else if (current_page_number == 3){
+        $("#page").append(
+           ' <div class="header_div" style="height: 180px;" >'+
+            ' <h1 class="headline" style="font-size: 58px; white-space: nowrap;"> Check out What Events are happening!</h1>'+
+            '</div>'+
+            '<div class="content_div" style="height:560px;">'+
+                '<div class="container">'+
+                    '<div class="item_container" style="margin-top:0px; margin-right:30px; height: 570px;">'+
+                        '<div class="events" type="img" style="background-image: url(images/ally.jpg);"></div>'+
+                        '<div class="item_title" style="padding-top: 20px; font-size: 70px; color: orange;"><p>ALLY Network</p></div>'+
+                            '<div class="events_description">'+
+                                '<div>Wednesday 28 Feb, 1-2 pm </div>'+
+                                '<p>Drop by and learn about UTS ALLY Network and how you can become an ALLY to support the LGBTQIA+ community at UTS </p>'+
+                            '</div>'+
+                    '</div>'+
+                    '<div class="item_container" style="margin-top:0px; margin-right:30px; margin-left: 30px; height: 570px;">'+
+                        '<div class="events" type="img" style="background-image: url(images/dragbingo.png);"></div>'+
+                        '<div class="item_title" style="padding-top: 20px; font-size: 70px; color: red;"><p>Drag Bingo</p></div>'+
+                        '<div class="events_description">'+
+                            '<div>Thursday 29 Feb, 12-1 pm</div>'+
+                            '<p>Drag Bingo will be hosted by the fabulous Ms Prada Clutch with prizes to be won.</p>'+
+                        '</div>'+
+                    '</div>'+
+                    '<div class="item_container" style="margin-top:0px; margin-left:30px; height: 570px;">'+
+                        '<div class="events" type="img" style="background-image: url(images/studentgroup.png);"></div>'+
+                        '<div class="item_title" style="color: green;"><p>LGBTQIA+ UTS Student Groups</p></div>'+
+                        '<div class="events_description">'+
+                            '<div>Friday 1 Mar, 1-2 pm</div>'+
+                            '<p>Come along and meet the UTS Darlings. Drop by and find out about these student Group</p>'+
+                        '</div>'+
+                    '</div>'+
+                '</div>'  +      
+            '</div>'
+            // '<div class="footer_div">'+
+            //     '<button class="button_home"></button>'+
+            // '</div>'
+        );
+    }
+    speak(texts[current_page_number-1]);
+    timeoutloop();
+}
+var timer;
+function timeoutloop(){
+    timer = setTimeout( function(){
+    convertPage(1);
+    }, 10000);
+}
+
+function speak(text){
+    if (makesound == true){
+        session.service("ALTextToSpeech").then(function(tts){
+            tts.say(text);
+        });
+    }
+}
+
+var makesound = false;
+function eventDonecallback_human_tracked(state){
+    if(state ==-1){
+        makesound = false;
+    }
+    else{
+        makesound = true;
+    }
+}
+session.service("ALMemory").then(function(memory){
+    memory.subscriber("ALBasicAwareness/HumanTracked").then(function(subscriber){
+        subscriber.signal.connect(eventDonecallback_human_tracked);
     });
-    $('button').click(function(){
-        $("#"+$(this).attr('id')).css('filter', 'invert(100)%');
+});
+function exit(){
+    session.service("ALBehaviorManager").then(function(behaviour){
+        behaviour.stopAllBehaviors();
+    });
+}
+$(document).ready( function(){
+    $(".button_home").click( function(){
+        audio.play();
+        exit();
     });
     
+    $('.arrow').click(function(){
+        audio.play();
+        if ($(this).attr("id") == "arrow_right"){
+            convertPage(1);
+        }
+        else{
+            convertPage(-1);
+        }
+    });
+    
+   
 });
